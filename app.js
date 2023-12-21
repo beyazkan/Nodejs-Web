ca9a73cfe06a1b94e8ea5025b5759bb89612f22c
Mustafa Sabri Oğuz <m.sabri.oguz@gmail.com>
2023-12-21 11:19:18 +0300

    File System Modülü ve Kullanımı

--- app.js
+++ app.js
@@ -1,10 +1,58 @@
-const url = require('url');
+// File System Modülü Dahil Etme
+const fs = require('fs');
 
-const address = "https://msoguz.com/kurslar?year=2019&month=nisan";
+/*
+const files = fs.readdir('./', function(error, data){
+    if(error){
+        console.log(error);
+    }else{
+        console.log(data);
+    }
+});
+*/
 
-let result = url.parse(address, true);
-
-console.log(result);
-console.log(result.path);
-console.log(result.query.year);
-console.log(result.query.month);
+/*
+const data = fs.readFile('index.html', 'utf-8', function(error, data){
+    if(error){
+        console.log(error);
+    }else{
+        console.log(data);
+    }
+});
+*/
+/* Dosya oluşturma ve üzerine yazma yöntemi
+fs.writeFile('deneme.txt', 'Hello World...', function(error){
+    if(error){
+        console.log(error);
+    }else{
+        console.log('Dosya oluşturuldu.');
+    }
+});
+*/
+/* Dosya oluşturma ve ekleme yapma yöntemi
+fs.appendFile('deneme1.txt', 'Hello World...', function(error){
+    if(error){
+        console.log(error);
+    }else{
+        console.log('Dosya oluşturuldu.');
+    }
+});
+*/
+/* Dosya silme yöntemi
+fs.unlink('deneme1.txt', function(error){
+    if(error){
+        console.log(error);
+    }else{
+        console.log('Dosya Silindi.');
+    }
+})
+*/
+/* Dosya ismi değiştirme yöntemi
+fs.rename('deneme.txt', 'deneme1.txt', function(error){
+    if(error){
+        console.log(error);
+    }else{
+        console.log('dosya ismi değiştirildi.');
+    }
+});
+*/
