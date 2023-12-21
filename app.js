f8e1aa1cc2af99333453c23fba00c3075176f792
Mustafa Sabri Oğuz <m.sabri.oguz@gmail.com>
2023-12-21 10:17:50 +0300

    Global Nesne Kavramı

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-// Programlamaya Başlayalım.
-console.log("Merhaba Dünya!");
+// Global Nesneler
+// Tarayıcı
+var firstName = "Mustafa";
+var lastName = "Oğuz";
+
+console.log("Window Objesi Tarayıcıda Global olarak bulunur.");
+//console.log(window);
+
+console.log(firstName);
+console.log(lastName);
+
+// Window nesnesi Global Nesnedir.
+// window.console.log();
+// window.setInterval;
+// window.setTimeout;
+// window.clearTimeout;
+
+// Nodejs
+
+// global
+console.log(global);
