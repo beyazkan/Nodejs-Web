7f30e130774a360547fb4934a0fc028c46f07418
mustafa.oguz <m.sabri.oguz@gmail.com>
2023-12-26 15:38:04 +0300

    Sending Response Kavramı

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
-const Logger = require('./logger.js');
-const logger = new Logger();
+const http = require('http');
+const fs = require('fs');
 
-// Listener kayıt et
-logger.on('connection', function(args){
-    console.log('bağlantı kuruldu.', args);
+const server = http.createServer();
+
+server.on('request', (req, res)=>{
+    //res.setHeader('Content-Type', 'text/plain');
+    //res.setHeader('Content-Type', 'application/json');
+    // res.setHeader('Content-Type', 'text/html');
+    // res.statusCode = 200;
+    // res.statusMessage = 'Ok';
+
+    //res.write('Hello World');
+    // res.write(JSON.stringify({name:'Samsung S8', price: 3000}));
+
+    // res.write('<html>');
+    // res.write('<head><title>Deneme</title></head>');
+    // res.write('<body>');
+    // res.write('<h1>Bu bir test yazisidir.</h1>');
+    // res.write('</body>');
+    // res.write('</html>');
+    // res.end();
+
+    fs.readFile('index.html', function(error, file) {
+        if(error){
+            res.setHeader('Content-Type', 'text/plain');
+            res.statusCode = 404;
+            res.statusMessage = 'Not Found';
+            res.end('Dosya bulunamadı.');
+        }else{
+            res.setHeader('Content-Type', 'text/html');
+            res.statusCode = 200;
+            res.statusMessage = 'Ok';
+            res.end(file);
+        }
+    });
+
 });
 
-logger.on('logout', function(){
-    console.log('bağlantı sonlandırıldı.');
-});
-// event'i tetikle
-logger.log('Mustafa Oğuz login oldu.');
-logger.emit('logout');
+server.listen(3000);
+console.log('Listening port on 3000');
+
