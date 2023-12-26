96de8d400536a45986b56db30cf7391a15c38f76
mustafa.oguz <m.sabri.oguz@gmail.com>
2023-12-26 16:25:50 +0300

    Routing Request Kavramı

--- app.js
+++ app.js
@@ -1,40 +1,33 @@
 const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
 
-server.on('request', (req, res)=>{
-    //res.setHeader('Content-Type', 'text/plain');
-    //res.setHeader('Content-Type', 'application/json');
-    // res.setHeader('Content-Type', 'text/html');
-    // res.statusCode = 200;
-    // res.statusMessage = 'Ok';
-
-    //res.write('Hello World');
-    // res.write(JSON.stringify({name:'Samsung S8', price: 3000}));
-
-    // res.write('<html>');
-    // res.write('<head><title>Deneme</title></head>');
-    // res.write('<body>');
-    // res.write('<h1>Bu bir test yazisidir.</h1>');
-    // res.write('</body>');
-    // res.write('</html>');
-    // res.end();
-
-    fs.readFile('index.html', function(error, file) {
-        if(error){
-            res.setHeader('Content-Type', 'text/plain');
-            res.statusCode = 404;
-            res.statusMessage = 'Not Found';
-            res.end('Dosya bulunamadı.');
-        }else{
-            res.setHeader('Content-Type', 'text/html');
-            res.statusCode = 200;
-            res.statusMessage = 'Ok';
-            res.end(file);
-        }
-    });
-
+    res.setHeader('Content-Type', 'text/html');
+    if(url === '/'){
+        res.write(`
+            <html>
+            <head>
+            <title>Enter Message</title>
+            </head>
+            <body>
+                <form method="POST" action="/log">
+                    <input type="text" name="message" />
+                    <button type="submit">Save</button>
+                </form>
+            </body>
+            </html>
+        `);
+        return res.end();
+    }
+    if(url === '/log' && method === 'POST'){
+        fs.writeFileSync('message.txt', 'deneme');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
 });
 
 server.listen(3000);
