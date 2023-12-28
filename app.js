6c8a827ae56ce1714f52e9a1ba594e0cf9f40538
mustafa.oguz <m.sabri.oguz@gmail.com>
2023-12-28 11:18:49 +0300

    Routes Modülü Kavramı

--- app.js
+++ app.js
@@ -1,52 +1,7 @@
 const http = require('http');
-const fs = require('fs');
-const qs = require('querystring');
+const routes = require('./routes.js');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-
-    res.setHeader('Content-Type', 'text/html');
-    if(url === '/'){
-        res.write(`
-            <html>
-            <head>
-            <title>Enter Message</title>
-            </head>
-            <body>
-                <form method="POST" action="/log">
-                    <input type="text" name="message" />
-                    <button type="submit">Save</button>
-                </form>
-            </body>
-            </html>
-        `);
-        return res.end();
-    }
-    if(url === '/log' && method === 'POST'){
-        // Body parsing()
-        const body = [];
-
-        req.on('data', (chunk) =>{
-            body.push(chunk);
-            console.log(chunk);
-        });
-
-        req.on('end', ()=>{
-            const bodyParsed = Buffer.concat(body).toString();
-            //console.log(bodyParsed);
-            const message = bodyParsed.split('=')[1]+'\n';
-            console.log(qs.parse(bodyParsed));
-
-            fs.appendFileSync('message.txt', message);
-            
-        });
-
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
 console.log('Listening port on 3000');
