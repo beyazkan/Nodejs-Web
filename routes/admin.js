6e99abf3c42537af120959ef462f936aef2d58e0
Mustafa Sabri Oğuz <m.sabri.oguz@gmail.com>
2024-01-01 16:18:10 +0300

    Güncelleme

--- routes/admin.js
+++ routes/admin.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
 
+const path = require('path');
 // Ön ek adres '/admin'
 const prefix_url = '/admin'
 
+// /admin/add-product=> GET
 router.get('/add-product', (req, res, next) => {
     res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
+// /admin/add-product=> POST
 router.post('/add-product', (req, res, next) => {
     // Database Kayıt
     console.log(req.body);
-
     res.redirect(prefix_url);
 });
-
 router.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname, '../', 'views', 'admin-index.html'));
 });
