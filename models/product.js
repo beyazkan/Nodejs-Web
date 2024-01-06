787e499942949d0874a4081fe37834a6928c8d84
Mustafa Sabri Oğuz <m.sabri.oguz@gmail.com>
2024-01-06 13:44:32 +0300

    Random Urun Id - Ürün seçimi

--- models/product.js
+++ models/product.js
@@ -1,13 +1,14 @@
 const products = [
-    {name:'Samsung S6', price: 2000, imageUrl: '1.jpg', description: 'İyi telefon'},
-    {name:'Samsung S7', price: 2000, imageUrl: '1.jpg', description: 'İyi telefon'},
-    {name:'Samsung S8', price: 2000, imageUrl: '1.jpg', description: 'İyi telefon'},
-    {name:'Samsung S9', price: 2000, imageUrl: '1.jpg', description: 'İyi telefon'}
+    {id:1231, name:'Samsung S6', price: 2000, imageUrl: '1.jpg', description: 'İyi telefon'},
+    {id:4212, name:'Samsung S7', price: 2000, imageUrl: '1.jpg', description: 'İyi telefon'},
+    {id:3421, name:'Samsung S8', price: 2000, imageUrl: '1.jpg', description: 'İyi telefon'},
+    {id:2332, name:'Samsung S9', price: 2000, imageUrl: '1.jpg', description: 'İyi telefon'}
 ];
 
 module.exports = class Product{
 
     constructor(name, price, imageUrl, description){
+        this.id = Math.floor(Math.random()*99999)+1;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
