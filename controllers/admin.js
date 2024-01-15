7e789aa3a1ef9e85dd8eab3fe58ab606a31ce325
mustafa.oguz <m.sabri.oguz@gmail.com>
2024-01-15 13:18:31 +0300

    Admin sayfalarında ürün kategorileri ayarlandı.

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product.js');
+const Category = require('../models/category.js');
 
 exports.getProducts = (req, res, next) => {
     const products = Product.getAll();
@@ -13,21 +14,23 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getAddProduct = (req, res, next) => {
+    const categories = Category.getAll();
     res.render('./admin/add-product.pug', {
         title: 'Ürün Ekle',
-        path: '/admin/add-product'
+        path: '/admin/add-product',
+        categories: categories
     });
 };
 
 exports.postAddProduct = (req, res, next) => {
     // Database Kayıt
     console.log(req.body);
-    const product = new Product(
-        req.body.name, 
-        req.body.price, 
-        req.body.imageUrl, 
-        req.body.description
-        );
+    const product = new Product();
+    product.name = req.body.name;
+    product.price = req.body.price;
+    product.imageUrl = req.body.imageUrl;
+    product.description = req.body.description;
+    product.categoryId = req.body.categoryid;
     product.saveProduct();
     //res.redirect(prefix_url);
     res.redirect('/');
@@ -35,11 +38,13 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getEditProduct = (req, res, next) => {
     const product = Product.getById(req.params.productid);
+    const categories = Category.getAll();
 
     res.render('./admin/edit-product.pug', {
         title: 'Ürün Düzenle',
         product: product,
-        path: '/admin/edit-product'
+        path: '/admin/edit-product',
+        categories: categories
     });
 };
 
@@ -51,6 +56,7 @@ exports.postEditProduct = (req, res, next) => {
     product.price = req.body.price;
     product.imageUrl = req.body.imageUrl;
     product.description = req.body.description;
+    product.categoryId = req.body.categoryid;
 
     Product.Update(product);
     
