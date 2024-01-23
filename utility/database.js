d33d383f156219a30c2fc138b6ac6c8ca487d971
Mustafa Sabri Oğuz <m.sabri.oguz@gmail.com>
2024-01-23 20:31:34 +0300

    Düzeltme

--- utility/database.js
+++ utility/database.js
@@ -3,7 +3,6 @@ const MongoClient = mongodb.MongoClient;
 
 const mongoConnect = (callback) => {
      MongoClient.connect('mongodb://127.0.0.1')
-    //MongoClient.connect('mongodb+srv://DarkGoku:654333@atlascluster.hbbl2nx.mongodb.net/')
         .then(client => {
             console.log('connected');
             callback(client);
