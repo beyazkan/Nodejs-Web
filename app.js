a2cea34f5fcf25527b10353113e938de0fed72df
mustafa.oguz <m.sabri.oguz@gmail.com>
2024-01-26 09:19:39 +0300

    Kullanıcı Eklenmesi

--- app.js
+++ app.js
@@ -15,8 +15,19 @@ const userRoutes = require('./routes/shop.js');
 const errorController = require('./controllers/errors.js');
 const mongoConnect = require('./utility/database.js').mongoConnect;
 
+const User = require('./models/user.js');
+
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req,res,next) => {
+    User.findByUserName('msoguz')
+    .then(user => {
+        req.user = new User(user.name, user.email, user._id);
+        next();
+    })
+    .catch(error => { console.log(error) })
+})
 
 // Routes
 app.use('/admin', adminRoutes);
@@ -26,5 +37,21 @@ app.use(userRoutes);
 app.use(errorController.get404Page);
 
 mongoConnect(() => {
-    app.listen(3000);
+    User.findByUserName('msoguz')
+    .then(user => {
+        if(!user){
+            user = new User('msoguz', 'msoguz@gmail.com');
+            return user.save();
+        }
+        return user;
+    })
+    .then(user => {
+        console.log(user);
+        app.listen(3000);
+    })
+    .catch(error => {
+        console.log(error);
+    })
+
+    
 });
--- models/user.js
+++ models/user.js
@@ -1,15 +1,39 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../utility/database.js');
+const getDB = require('../utility/database').getDatabase;
+const mongodb = require('mongodb');
 
-const User = sequelize.define('user', {
-    id:{
-        type:Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    name: Sequelize.STRING,
-    email: Sequelize.STRING
-});
+class User{
+    constructor(name, email, id){
+        this.name = name;
+        this.email = email;
+        this._id = id;
+    }
+
+    save(){
+        const db = getDB();
+        return db.collection('users').insertOne(this);
+    }
+
+    static findById(userid){
+        const db = getDB();
+        return db.collection('users').findOne({_id: new mongodb.ObjectId(userid)})
+        .then(user => {
+            return user;
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+
+    static findByUserName(username){
+        const db = getDB();
+        return db.collection('users').findOne({name: username})
+        .then(user => {
+            return user;
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+}
 
 module.exports = User;
