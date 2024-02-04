50b11f228adf4a100b7cc080bf4cfe3a036a2526
Mustafa Sabri Oğuz <m.sabri.oguz@gmail.com>
2024-02-04 21:16:07 +0300

    Session Veritabanında Saklanması

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const mongoDbStore = require('connect-mongodb-session')(session);
 
 const bodyParser = require('body-parser');
 
@@ -20,14 +21,28 @@ const errorController = require('./controllers/errors.js');
 //const mongoConnect = require('./utility/database.js').mongoConnect;
 
 const User = require('./models/user.js');
+const connectionString = 'mongodb://127.0.0.1/node-app';
+var store = new mongoDbStore({
+    uri: connectionString,
+    collection: 'mySessions'
+});
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(cookieParser());
 app.use(session({
     secret: 'keyboard cat',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7 // 1 Haftalık
+    },
+    store: store
 }));
+
+store.on('error', function(error) {
+    console.log(error);
+  });
+  
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req,res,next) => {
@@ -49,7 +64,7 @@ app.use(accountRoutes);
 // 404 Hatası
 app.use(errorController.get404Page);
 
-mongoose.connect('mongodb://127.0.0.1/node-app')
+mongoose.connect(connectionString)
     .then(() => {
         console.log('Connected to mongodb');
         User.findOne({name: 'msoguz'})
