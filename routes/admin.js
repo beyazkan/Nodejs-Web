bfdb8a88f20dd7ac5ed3c6af27f3a6cf93e888ef
mustafa.oguz <m.sabri.oguz@gmail.com>
2024-02-06 10:38:17 +0300

    Route Protection

--- routes/admin.js
+++ routes/admin.js
@@ -1,34 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/admin.js');
+const isAuthenticated = require('../middleware/authentication.js');
 
 // Ön ek adres '/admin'
 const prefix_url = '/admin'
 
 //admin/add-product=> GET
-router.get('/add-product', adminController.getAddProduct);
+router.get('/add-product', isAuthenticated, adminController.getAddProduct);
 //admin/add-product=> POST
-router.post('/add-product', adminController.postAddProduct);
+router.post('/add-product', isAuthenticated, adminController.postAddProduct);
 
 //admin/edit-product=> GET
-router.get('/edit-product/:productid', adminController.getEditProduct);
+router.get('/edit-product/:productid', isAuthenticated, adminController.getEditProduct);
 //admin/edit-product=> POST
 
-router.post('/edit-product', adminController.postEditProduct);
-router.get('/products/:productid', adminController.getEditProduct);
+router.post('/edit-product', isAuthenticated, adminController.postEditProduct);
+router.get('/products/:productid', isAuthenticated, adminController.getEditProduct);
 
-router.get('/products', adminController.getProducts);
+router.get('/products', isAuthenticated, adminController.getProducts);
 
-router.post('/delete-product', adminController.postDeleteProduct);
+router.post('/delete-product', isAuthenticated, adminController.postDeleteProduct);
 
 // Kategori Sayfaları
-router.get('/add-category', adminController.getAddCategory);
-router.post('/add-category', adminController.postAddCategory);
-router.get('/edit-category/:categoryid', adminController.getEditCategory);
-router.post('/edit-category', adminController.postEditCategory);
-router.post('/delete-category', adminController.postDeleteCategory);
-router.get('/categories', adminController.getCategories);
+router.get('/add-category', isAuthenticated, adminController.getAddCategory);
+router.post('/add-category', isAuthenticated, adminController.postAddCategory);
+router.get('/edit-category/:categoryid', isAuthenticated, adminController.getEditCategory);
+router.post('/edit-category', isAuthenticated, adminController.postEditCategory);
+router.post('/delete-category', isAuthenticated, adminController.postDeleteCategory);
+router.get('/categories', isAuthenticated, adminController.getCategories);
 
-router.get('/', adminController.adminIndex);
+router.get('/', isAuthenticated, adminController.adminIndex);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -2,16 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const shopController = require('../controllers/shop.js');
+const isAuthenticated = require('../middleware/authentication.js');
 
 router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
 router.get('/product/details/:productid', shopController.getProduct);
 router.get('/categories/:categoryid', shopController.getProductsByCategoryId);
-router.get('/cart', shopController.getCart);
-router.post('/cart', shopController.postCart);
-router.post('/delete-cartItem', shopController.postCartItemDelete);
-router.get('/orders', shopController.getOrders);
-router.post('/create-order', shopController.postOrder);
+router.get('/cart', isAuthenticated, shopController.getCart);
+router.post('/cart', isAuthenticated, shopController.postCart);
+router.post('/delete-cartItem', isAuthenticated, shopController.postCartItemDelete);
+router.get('/orders', isAuthenticated, shopController.getOrders);
+router.post('/create-order', isAuthenticated, shopController.postOrder);
 
 
 module.exports = router;
